68f712d17df4716d81ae40f52ea20bd65c161a9a
unknown <tpratiksha692@gmail.com>
2020-09-20 18:42:37 +0530

    Pratiksha commit

---
+++ webapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'index.html',{})
+def register(request):
+	return render(request, 'register.html',{})
+def login(request):
+	return render(request, 'login.html',{})
+def elements(request):
+	return render(request, 'elements.html',{})
+def courses(request):
+	return render(request, 'courses.html',{})
+def contact(request):
+	return render(request, 'contact.html',{})
+def blog_details(request):
+	return render(request, 'blog_details.html',{})
+def blog(request):
+	return render(request, 'blog.html',{})
+def about(request):
+	return render(request, 'about.html',{})
